124d5ecb4c47577eb7b13a662af3fac322647a9f
Connor Campbell <ccampbell.james@gmail.com>
2024-05-01 11:01:24 +0100

    feat: add logging

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,8 @@
-use lambda_runtime::{run, service_fn, tracing, Error, LambdaEvent};
+use lambda_runtime::{
+    run, service_fn,
+    tracing::{self, error, info},
+    Error, LambdaEvent,
+};
 use octocrab::models::issues::Issue;
 use reqwest::{
     header::{HeaderMap, ETAG, USER_AGENT},
@@ -6,20 +10,21 @@ use reqwest::{
 };
 use serde::{Deserialize, Serialize};
 
-#[derive(Deserialize)]
+#[derive(Deserialize, Debug)]
 struct Request {
     owner: String,
     repo: String,
     issue_number: u64,
 }
 
-#[derive(Serialize)]
+#[derive(Serialize, Debug)]
 struct LResponse {
     etag: String,
     issue: Issue,
 }
 
 async fn function_handler(event: LambdaEvent<Request>) -> Result<LResponse, Error> {
+    info!("Event payload: {:?}", event.payload);
     let owner = event.payload.owner;
     let repo = event.payload.repo;
     let issue_number = event.payload.issue_number;
@@ -35,12 +40,19 @@ async fn function_handler(event: LambdaEvent<Request>) -> Result<LResponse, Erro
         let etag = response
             .headers()
             .get(ETAG)
-            .ok_or_else(|| Error::from("ETag header is missing in the response"))?
+            .ok_or_else(|| {
+                error!("ETag header is missing in the response");
+                Error::from("ETag header is missing in the response")
+            })?
             .to_str()
-            .map_err(|_| Error::from("Failed to convert ETag header to string"))?
+            .map_err(|_| {
+                error!("Failed to convert ETag header to string");
+                Error::from("Failed to convert ETag header to string")
+            })?
             .to_owned();
 
         let issue: Issue = response.json().await.map_err(|err| {
+            error!("Failed to deserialize issue from response: {}", err);
             Error::from(format!(
                 "Failed to deserialize issue from response: {}",
                 err
@@ -49,6 +61,7 @@ async fn function_handler(event: LambdaEvent<Request>) -> Result<LResponse, Erro
 
         let resp = LResponse { etag, issue };
 
+        info!("Response: {:?}", resp);
         Ok(resp)
     } else {
         Err(Error::from("Failed to fetch issue"))
